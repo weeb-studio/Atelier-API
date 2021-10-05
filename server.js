220d7d9968e8d4215678450c15017a41801e083d
Patrick23718 <noukimi.patrick@gmail.com>
2021-10-05 17:46:08 +0100

    05/10

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 3080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
