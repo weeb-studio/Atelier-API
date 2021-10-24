d5d10c9a812f2ac942ec0833f71aa09ee9a167f1
Patrick23718 <noukimi.patrick@gmail.com>
2021-10-24 01:25:41 +0100

    23/10

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const Role = db.role;
 app.use(helmet());
 app.disable("x-powered-by");
 
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    parameterLimit: 100000,
+    extended: true,
+  })
+);
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
