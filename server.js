90d8e17ae023348662b6703899d7746b8122fb44
Patrick23718 <noukimi.patrick@gmail.com>
2022-05-17 15:48:34 +0000

    new

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = 3000;
+const PORT = 3500;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
